3cb1f67cb67b8e28e76fd79c92503dd54920bcb0
kirspivak <kirspivak123@gmail.com>
2025-05-29 15:23:17 +0300

    исправить имя переменной окружения для ключа Django приложения 2

--- payments_system/payments_system/settings.py
+++ payments_system/payments_system/settings.py
@@ -25,7 +25,7 @@ load_dotenv(BASE_DIR.parent / ".env")
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('DJANGO_SECREY_KEY')
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
