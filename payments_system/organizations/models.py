433f32c48c99bbfef97e8536faf4acbf9033e2a6
kirspivak <kirspivak123@gmail.com>
2025-05-29 15:06:52 +0300

    добавить модель Organization

--- payments_system/organizations/models.py
+++ payments_system/organizations/models.py
@@ -1,3 +1,20 @@
+import uuid
+
 from django.db import models
 
-# Create your models here.
+
+class Organization(models.Model):
+    id = models.UUIDField(
+        primary_key=True, 
+        default=uuid.uuid4,
+    )
+    inn = models.CharField(
+        'ИНН',
+        max_length=12,
+        unique=True,
+    )
+    balance = models.DecimalField(
+        'Баланс',
+        max_digits=14,
+        decimal_places=2,
+    )
