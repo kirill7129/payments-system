2bbe5c9e91925152074202f6c164cdeab545bece
kirspivak <kirspivak123@gmail.com>
2025-05-29 14:25:01 +0300

    вынести секретный ключ Django приложения в переменные окружения и указать в шаблоне необходимость его наличия

--- payments_system/payments_system/settings.py
+++ payments_system/payments_system/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3fje)i_&*08e!oa9_1=_4g-*a96bdtxs=$i3$36@9o2&2=&-4g'
+SECRET_KEY = os.environ.get('DJANGO_SECREY_KET')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
